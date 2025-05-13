de341249d967291e33ad0d979780984768ba629b
nguyenvanquang97 <quangdtkc@gmail.com>
2025-05-13 16:29:32 +0700

    feat(FAQs): add FAQs service and integrate with FAQ component
    Add a new service `FAQsServices.js` to fetch FAQs data from the backend. Update the FAQ component to use this service, decode HTML entities, and dynamically render FAQs. Also, add necessary dependencies (`html-entities` and `react-html-parser`) to support HTML content parsing and decoding.
    
    refactor(FilterTours): enhance tour item UI and add hover effects
    
    Improve the UI of tour items by adding hover effects, lazy loading for images, and replacing buttons with links for better navigation. Additionally, hide the ExploreTopTravelService image by default.
    
    style(PlainYourTrip): improve responsive design and layout
    
    Adjust the layout and styling of the PlainYourTrip component to enhance responsiveness, especially on smaller screens. This includes flexbox adjustments, padding modifications, and text size optimizations.
    
    chore: update package dependencies
    
    Add `html-entities` and `react-html-parser` to package.json and package-lock.json to support the new FAQs feature.

--- src/components/PlainYourTrip.jsx
+++ src/components/PlainYourTrip.jsx
@@ -16,34 +16,34 @@ const PlainYourTrip = ({ className = '' }) => {
                 className
             )}
         >
-            <div className="items-center flex justify-between p-[15px_15px_15px_50px]">
-            <Form action="/tripdetail" className="h-fit">
-                <h2 className="text-[40px] font-bold mb-[20px]">
-                    Plan Your Trip
-                </h2>
-                <p className="text-[20px] font-normal mb-[30px]">
-                    Plan your trip for free and enjoy exclusive deals on local experiences!
-                </p>
-                <InputTripLocation />
-                <CreateItineraryButton className="mt-[12px]" />
-                <div className="flex gap-[12px] mt-[30px]">
-                    <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_15px]">
-                        <img src={iconVisa} alt="Visa" />
-                        Visas
-                    </button>
-                    <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_15px]">
-                        <img src={iconItinerary} alt="Visa" />
-                        Itineraries
-                    </button>
-                    <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_15px]">
-                        <img src={iconTools} alt="Visa" />
-                        Apps & Tools
-                    </button>
+            <div className="flex flex-col lg:flex-row items-center justify-between p-4 lg:p-[15px_15px_15px_50px] gap-8">
+                <Form action="/tripdetail" className="h-fit w-full lg:w-1/2">
+                    <h2 className="text-[28px] md:text-[32px] lg:text-[40px] font-bold mb-[15px] lg:mb-[20px]">
+                        Plan Your Trip
+                    </h2>
+                    <p className="text-[16px] md:text-[18px] lg:text-[20px] font-normal mb-[20px] lg:mb-[30px]">
+                        Plan your trip for free and enjoy exclusive deals on local experiences!
+                    </p>
+                    <InputTripLocation />
+                    <CreateItineraryButton className="mt-[12px] mx-auto lg:mx-0"  />
+                    <div className="flex  justify-center lg:justify-start  flex-wrap gap-[8px] md:gap-[12px] mt-[20px] lg:mt-[30px]">
+                        <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_12px] md:p-[5px_15px] text-[14px] md:text-[16px]">
+                            <img src={iconVisa} alt="Visa" />
+                            Visas
+                        </button>
+                        <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_12px] md:p-[5px_15px] text-[14px] md:text-[16px]">
+                            <img src={iconItinerary} alt="Visa" />
+                            Itineraries
+                        </button>
+                        <button className="flex gap-[5px] bg-white rounded-[80px] p-[5px_12px] md:p-[5px_15px] text-[14px] md:text-[16px]">
+                            <img src={iconTools} alt="Visa" />
+                            Apps & Tools
+                        </button>
+                    </div>
+                </Form>
+                <div className="w-full lg:w-1/2">
+                    <img src={image} alt="Map" className="w-full max-w-[480px] mx-auto" loading="lazy" />
                 </div>
-            </Form>
-            <div>
-                <img src={image} alt="Map" width="480px" loading="lazy" />
-            </div>
             </div>
         </section>
     )
