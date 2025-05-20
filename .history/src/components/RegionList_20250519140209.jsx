0a3218298adf8a7540f3f59fe10f2f1fa23f5a22
hoangnv <ngvhoang6886@gmail.com>
2025-05-20 08:08:53 +0700

    2025_05_19_bak_com_afternoon_again

---
+++ .history/src/components/RegionList_20250519140209.jsx
@@ -0,0 +1,177 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@ui/carousel";
+import imageNaNoi from "@images/image-hanoi.png";
+import imageHaLong from "@images/image-halong.png";
+import imageSapa from "@images/image-sapa.png";
+import { NavLink } from "react-router";
+import { FaArrowRight } from "react-icons/fa6";
+
+import NorthImage from "./NorthImage";
+import CentralImage from "./CentralImage";
+import SouthImage from "./SouthImage";
+import ParacelIslands from "./ParacelIslands";
+import SpratlyIslands from "./SpratlyIslands";
+import { cn } from "@lib/utils";
+import { useState } from "react";
+const RegionList = ({ className, ...props }) => {
+    const curentRegion = "North";
+    const desRegion =
+        "West Vietnam features a distinctive landscape of rugged mountains, lush forests, and beautiful waterfalls. It also offers historical landmarks like the famous River Kwai Bridge, ancient temples, and intriguing cultural heritage sites, making it a diverse and captivating travel destination.";
+    const [activeRegion, setActiveRegion] = useState(0);
+    return (
+        <section {...props} className={cn("container", className)}>
+            {/* Menu Region */}
+            <div className="text-center">
+                <h2 className="text-[#1A2A44] text-[40px] font-bold mb-[24px]">
+                    Destination by Region
+                </h2>
+                <ul className="inline-flex items-center justify-center gap-[46px] rounded-[80px] bg-white shadow-[0px_4px_12px_0px_rgba(54,133,143,0.15)] px-[70px] py-[16px] pb-[20px] mb-[66px]">
+                    {[
+                        "North",
+                        "Central",
+                        "South",
+                        "Paracel Islands",
+                        "Spratly Islands",
+                    ].map((region, index) => (
+                        <li>
+                            <NavLink
+                                key={index}
+                                onClick={(e) => {
+                                    console.log(index);
+                                    e.preventDefault();
+                                    setActiveRegion(index);
+                                }}
+                                className={`relative ${
+                                    activeRegion === index
+                                        ? "hnv_region_active_menu"
+                                        : ""
+                                }
+                                    `}
+                            >
+                                {region}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flex flex-col lg:flex-row items-center gap-[35px]">
+                {/* Bản đồ vùng - chỉ hiển thị desktop */}
+                <div className="hidden lg:block min-w-[512px] relative h-fit">
+                    <div>
+                        <NorthImage className="hnv_region_active_map" />
+                    </div>
+                    <div className="translate-x-[69px] translate-y-[-32px]">
+                        <CentralImage className="hnv_region_active_map" />
+                    </div>
+                    <div className="translate-x-[93px] translate-y-[-44px]">
+                        <SouthImage />
+                    </div>
+                    <div className="absolute top-[282px] right-[82px]">
+                        <ParacelIslands />
+                    </div>
+                    <div className="absolute top-[528px] right-[12px]">
+                        <SpratlyIslands />
+                    </div>
+                </div>
+
+                {/* Thông tin + Carousel */}
+                <div className="w-full text-left">
+                    <h2 className="text-[#1A2A44] text-[40px] md:text-[56px] lg:text-[72px] font-bold">
+                        {curentRegion}
+                    </h2>
+                    <p className="text-[#494951] text-[16px] md:text-[18px] lg:text-[20px] font-normal mb-[30px] md:mb-[40px] lg:mb-[50px]">
+                        {desRegion}
+                    </p>
+
+                    <Carousel
+                        className="w-full"
+                        opts={{
+                            align: "start",
+                            skipSnaps: false,
+                            containScroll: "trimSnaps",
+                            loop: false,
+                            dragFree: false,
+                        }}
+                    >
+                        <CarouselContent className="-ml-[20px]">
+                            {[imageNaNoi, imageHaLong, imageSapa].map(
+                                (img, index) => {
+                                    const titles = ["Hanoi", "Halong", "Sa pa"];
+                                    return (
+                                        <CarouselItem
+                                            key={index}
+                                            className="basis-[80%] sm:basis-[45%] pl-[20px] relative group"
+                                        >
+                                            <NavLink to="/city/hanoi">
+                                                <img
+                                                    src={img}
+                                                    alt={titles[index]}
+                                                    className="w-full rounded-[60px_4px_4px_4px]"
+                                                    loading="lazy"
+                                                />
+                                                <div
+                                                    className={cn(
+                                                        "absolute right-0 top-[calc(100%-70px)] z-10 w-[calc(100%-20px)]",
+                                                        "p-[20px_20px_0_20px] overflow-hidden transition-all duration-500",
+                                                        "group-hover:top-[calc(100%-195px)]"
+                                                    )}
+                                                >
+                                                    <h3 className="text-white text-[28px] font-bold">
+                                                        {titles[index]}
+                                                    </h3>
+                                                    <div
+                                                        className={cn(
+                                                            "opacity-0 group-hover:opacity-100",
+                                                            "transition-all duration-500"
+                                                        )}
+                                                    >
+                                                        <p className="text-white mt-2 font-[16px]">
+                                                            Many desktop
+                                                            publishing packages
+                                                            and web page editors
+                                                            now use Lorem Ipsum
+                                                            as their default
+                                                            model text,
+                                                        </p>
+                                                        <div
+                                                            className={cn(
+                                                                "flex mt-[10px] mb-[20px] text-[17px] font-bold",
+                                                                "text-white gap-[8px] items-center justify-end"
+                                                            )}
+                                                        >
+                                                            <span>
+                                                                Discover
+                                                            </span>
+                                                            <FaArrowRight />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div
+                                                    className={cn(
+                                                        "rounded-[4px] bg-[linear-gradient(180deg,rgba(4,18,58,0)_0%,rgba(4,18,58,0.5)_100%)]",
+                                                        "absolute bottom-0 right-0 w-[calc(100%-20px)] h-[145px] group-hover:h-[260px] z-0",
+                                                        "transition-all duration-500"
+                                                    )}
+                                                ></div>
+                                            </NavLink>
+                                        </CarouselItem>
+                                    );
+                                }
+                            )}
+                        </CarouselContent>
+                        <CarouselPrevious className="hidden md:flex left-[10px] md:left-[20px] cursor-pointer" />
+                        <CarouselNext className="hidden md:flex right-[10px] md:right-[20px] cursor-pointer" />
+                    </Carousel>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default RegionList;
---
+++ .history/src/pages/Home_20250519100916.jsx
@@ -0,0 +1,161 @@
+import heroImage from "@images/hero-image.webp";
+import { useTranslation } from "react-i18next";
+import Story from "@components/Story";
+import PlainYourTrip from "@components/PlainYourTrip";
+import TrendingItinerary from "@components/TrendingItinerary";
+import TopVietnamExperiences from "@components/TopVietnamExperiences";
+import VietNamEvent from "@components/VietNamEvent";
+import { useEffect, useRef, useState } from "react";
+import { Skeleton } from "@ui/skeleton";
+import { NavLink } from "react-router";
+import RegionList from "@components/RegionList";
+import TravelOffers from "@components/TravelOffers";
+import { cn } from "@lib/utils";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { FaArrowDown } from "react-icons/fa6";
+
+export default function Home() {
+    const { t } = useTranslation();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const imgRef = useRef(null);
+
+    const scrollDown = () => {
+        const regionElement = document.getElementById("region");
+        const heightHeader = document.querySelector("header").clientHeight;
+        window.scrollTo({
+            top: regionElement.offsetTop - heightHeader,
+            behavior: "smooth",
+        });
+    };
+
+    useEffect(() => {
+        if (imgRef.current.complete) {
+            setIsLoaded(true);
+        }
+    }, []);
+
+    return (
+        <main>
+            <section className="relative text-white h-screen max-w-screen overflow-hidden -mt-[75.45px] lg:-mt-[96px]">
+                {!isLoaded && (
+                    <Skeleton
+                        className={cn(
+                            "h-screen w-full rounded-0",
+                            "bg-[#989797] rounded-none"
+                        )}
+                    />
+                )}
+                <img
+                    rel="preload"
+                    loading="eager"
+                    decoding="async"
+                    src={heroImage}
+                    alt={t("home.hero_heading")}
+                    style={{ opacity: isLoaded ? 1 : 0 }}
+                    ref={imgRef}
+                    onLoad={() => setIsLoaded(true)}
+                    className="h-screen w-full absolute top-0 left-1/2 -translate-x-1/2 object-cover"
+                />
+                <div className="absolute bottom-[80px] lg:bottom-[120px] text-center w-full px-[20px]">
+                    <motion.h1
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={
+                            isLoaded
+                                ? { opacity: 1, y: 0 }
+                                : { opacity: 0, y: 100 }
+                        }
+                        transition={{ duration: 0.7, delay: 0.2 }}
+                        className={cn(
+                            "text-center text-[25px] md:text-[40px] lg:text-[50px] xl:text-[60px]",
+                            "text-shadow-[0_2px_4px_rgba(0_0_0_/_0.40)]",
+                            "w-full font-bold px-[20px] mb-[5px]"
+                        )}
+                    >
+                        {t("home.hero_heading")}
+                    </motion.h1>
+                    <motion.p
+                        layout
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={
+                            isLoaded
+                                ? { opacity: 1, y: 0 }
+                                : { opacity: 0, y: 100 }
+                        }
+                        transition={{ duration: 0.7, delay: 0.4 }}
+                        className={cn(
+                            "text-center text-[16px] lg:text-[20px] font-normal px-[20px]",
+                            "w-full container mx-auto mb-[20px] lg:mb-[50px]"
+                        )}
+                    >
+                        {t("home.description")}
+                    </motion.p>
+                    <motion.div
+                        className={cn(
+                            "flex gap-[15px] flex-wrap",
+                            "translate-t-[-50%] w-full justify-center"
+                        )}
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={
+                            isLoaded
+                                ? { opacity: 1, y: 0 }
+                                : { opacity: 0, y: 100 }
+                        }
+                        transition={{ duration: 0.7, delay: 0.5 }}
+                    >
+                        <NavLink
+                            to="/"
+                            className={cn(
+                                "transition-all duration-500 text-[14px] lg:text-[16px]",
+                                "p-[10px] lg:p-[15px] border-white border-2 rounded-[80px]",
+                                "hover:bg-[#153b33] hover:border-[#153b33] focus:ring-4",
+                                "focus:outline-none focus:ring-blue-300"
+                            )}
+                        >
+                            Plan Your Trip
+                        </NavLink>
+                        <NavLink
+                            to="/"
+                            className={cn(
+                                "transition-all duration-500 text-[14px] lg:text-[16px]",
+                                "p-[10px] lg:p-[15px] border-white border-2 rounded-[80px]",
+                                "hover:bg-[#153b33] hover:border-[#153b33] focus:ring-4",
+                                "focus:outline-none focus:ring-blue-300"
+                            )}
+                        >
+                            Explore Destinations
+                        </NavLink>
+                        <NavLink
+                            to="/"
+                            className={cn(
+                                "transition-all duration-500 text-[14px] lg:text-[16px]",
+                                "p-[10px] lg:p-[15px] border-white border-2 rounded-[80px]",
+                                "hover:bg-[#153b33] hover:border-[#153b33] focus:ring-4",
+                                "focus:outline-none focus:ring-blue-300"
+                            )}
+                        >
+                            Find Experiences
+                        </NavLink>
+                    </motion.div>
+                </div>
+                <FaArrowDown
+                    onClick={scrollDown}
+                    className={cn(
+                        "cursor-pointer absolute bottom-[15px]",
+                        "left-1/2 translate-[-50%] text-[24px]"
+                    )}
+                />
+            </section>
+            <RegionList
+                id="region"
+                className="lg:mt-[135px] mt-[80px] lg:mb-[60px] mb-[60px]"
+            />
+            <VietNamEvent className="bg-[#F5F6FA] py-[48px] lg:py-[80px] pt-[48px] lg:pt-[80px]" />
+            <TopVietnamExperiences className="mb-[80px] lg:mb-[120px] pt-[60px] lg:pt-[80px]" />
+            <TrendingItinerary className="mb-[80px] lg:mb-[120px]" />
+            <TravelOffers className="mb-[60px] lg:mb-[120px]" />
+            <PlainYourTrip className="mb-[80px] lg:mb-[120px]" />
+            <Story />
+        </main>
+    );
+}
