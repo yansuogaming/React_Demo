8b8f113ab243c9520dd18d6f1967e7455baee13e
thuanvp012 <thuanvp012van@gmail.com>
2025-05-24 08:23:07 +0700

    Fix Events

--- Frontend/src/pages/Events.jsx
+++ Frontend/src/pages/Events.jsx
@@ -13,9 +13,6 @@ import { cn, debounce } from "@lib/utils";
 import { CiLocationOn } from "react-icons/ci";
 import { IoTicketOutline } from "react-icons/io5";
 
-import { CiLocationOn } from "react-icons/ci";
-import { IoTicketOutline } from "react-icons/io5";
-
 const categories = [
     "All",
     "Online",
