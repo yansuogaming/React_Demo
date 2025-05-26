6e93979c514806fbae19da18c65eefccaa8f9072
Tech10.VietISO <tech10.vietiso@gmail.com>
2025-05-26 16:13:43 +0700

    fixbug hotel

--- Frontend/src/pages/Hotel.jsx
+++ Frontend/src/pages/Hotel.jsx
@@ -7,64 +7,80 @@ import WhyVisit from '@components/WhyVisit'
 import Reveal from '@components/animation/Reveal'
 import TextNormal from '@components/text/TextNormal'
 import { Button } from '@components/ui/button'
-// import image from '@images/hero-image-itineraries.png'
-// import { useTranslation } from 'react-i18next'
 // import star_white from '@images/star_white.svg'
-import FilterToursAttractions from '@components/FilterToursAttractions'
+import FilterToursAttractions from '@components/FilterToursAttractions';
+import calendar from '@images/calendar.png';
+import ic_room from '@images/ic_room.png';
+import search from '@images/search.png';
+import img_banner from '@images/img_banner.png';
+import { useTranslation } from 'react-i18next'
+import FilterHotel from '@components/FilterHotel'
+import RecentlyViewed from '@components/RecentlyViewed'
+import IsoCMSTestimonials from '@components/IsoCMSTestimonials'
 
 const Hotel = () => {
-    // const { t } = useTranslation();
-
-    // const breadcrumdItems = [
-    //     { label: t('home'), href: '/' },
-    //     { label: t('plan_your_trip'), href: '/' },
-    //     { label: t('attractions'), href: '/' },
-    // ]
+    const { t } = useTranslation();
+    const breadcrumdItems = [
+        { label: t('home'), href: '/' },
+        { label: t('plan_your_trip'), href: '/' },
+        { label: t('attractions'), href: '/' },
+    ]
 
     return (
-        <div>9999</div>
-        // <main>
-        //     <section className="container mb-[80px]">
-        //         <Breadcrumb
-        //             className="mb-[30px] mt-[15px]"
-        //             items={breadcrumdItems}
-        //         />
-        //         <Reveal className="grid grid-cols-1 lg:grid-cols-2 gap-[30px] items-center mt-[80px] bg-white rounded-tl-[60px] rounded-br-[60px] shadow-[0px_2px_8px_0px_rgba(0,0,0,0.10)]">
-        //             <div className=''>
-        //                 <img className='w-full' src={image} alt="Vietnam – Journeys You Simply Can’t Miss" />
-        //             </div>
-        //             <div className='mx-5 mb-5 lg:mx-0 lg:mb-0'>
-        //                 <h1 className="text-[#1A2A44] font-bold text-[30px] md:text-[40px]">
-        //                     Hanoi Attractions
-        //                 </h1>
+        <main>
+            <section className="">
+                <div className='relative w-full h-[400px]'>
+                    <img className='w-full h-full object-cover' src={img_banner} alt="Banner" />
+                    <div className='absolute inset-0 flex justify-center items-center'>
+                        <h1 className='text-white font-bold text-[56px]'>Vietnam Hotels</h1>
+                    </div>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <div className='w-full h-[50px] bg-white rounded-tl-[20px] rounded-tr-[20px] mt-[-25px] absolute items-center justify-center' />
 
-        //                 <TextNormal className="text-[14px] md:text-[16px]">
-        //                     Check out must-see sights and activities:
-        //                 </TextNormal>
-        //                 <TextNormal className="text-[14px] md:text-[16px] font-bold">
-        //                     Old Quarter, Hoa Lo Prison, Multi-day Tours, Flea & Street Markets. For personalised
-        //                 </TextNormal>
-        //                 <TextNormal className="text-[14px] md:text-[16px]">
-        //                     recommendations, try our AI trip-planning product.
-        //                 </TextNormal>
+                    <div className='w-full sm:w-[90%] md:w-[1282px] max-w-full h-[80px] sm:h-[100px] md:h-[111px] max-h-full bg-white shadow-2xl absolute justify-center items-center mt-[-70px] rounded-2xl px-[20px] flex flex-row gap-x-4'>
+                        <div className='w-[426px] h-[80px] border border-[#DADFE6] rounded-[8px] flex flex-col pt-2 pl-4'>
+                            <text className='font-[Helvetica_Neue]'>Search for accommodation</text>
+                            <input
+                                type="text"
+                                value={''}
+                                onChange={() => { }}
+                                placeholder="Find a hotel"
+                               className="mt-2 placeholder-gray-500 placeholder-opacity-50 placeholder:text-lg font-[Helvetica_Neue]"
+                            />
 
-        //                 <div className='w-[220px] h-[47px] bg-blue-500 rounded-4xl mt-[25px] flex justify-center items-center'>
-        //                     <img
-        //                         src={star_white}
-        //                         className="w-[18px] h-[18px] mr-[5px]"
-        //                     />
-        //                     <TextNormal className='text-[#FFFFFF] text-[14px] md:text-[16px] text-center'>Create itinerary now!</TextNormal>
-        //                 </div>
-        //             </div>
-        //         </Reveal>
-        //     </section>
-        //     {/* <MostPopularTours /> */}
-        //     <FilterToursAttractions className="mt-[80px] mb-[160px]" />
-        //     {/* <WhyVisit /> */}
-          
-        //     <PlainYourTrip className="mt-[120px]" />
-        //     <FAQ className="mt-[80px]"/>
-        // </main>
+                        </div>
+                        <div className='w-[333px] h-[80px] border border-[#DADFE6] rounded-[8px] flex flex-row pt-2 pl-4 justify-between'>
+                            <div className='flex flex-col'>
+                                <text className='font-[Helvetica_Neue]'>Check-in & Check-out Dates</text>
+                                <text className='font-[Helvetica_Neue] mt-2'>July 25, 2024 - July 27, 2024</text>
+                            </div>
+                            <img src={calendar} className='w-6 h-6 justify-center items-center mr-3 mt-5' />
+
+                        </div>
+                        <div className='w-[333px] h-[80px] border border-[#DADFE6] rounded-[8px] flex flex-row pt-2 pl-4 justify-between'>
+                            <div className='flex flex-col'>
+                                <text className='font-[Helvetica_Neue]'>Guests & Rooms</text>
+                                <text className='font-[Helvetica_Neue] mt-2'>1 Adult(s), 0 Child, 1 Room</text>
+                            </div>
+                            <img src={ic_room} className='w-6 h-6 justify-center items-center mr-3 mt-5' />
+
+                        </div>
+                        <div className='w-[118px] h-[80px] border border-[#18BABD] rounded-[8px] flex flex-row justify-center items-center'>
+                            <img src={search} className='w-6 h-6' />
+                            <text className='font-[Helvetica_Neue] text-[#18BABD] text-[16px] ml-2'>Search</text>
+                        </div>
+                    </div>
+                </div>
+                <Breadcrumb
+                    className="mt-9 ml-18"
+                    items={breadcrumdItems}
+                />
+            </section>
+            <FilterHotel className="mt-[40px] mb-[160px]" />
+            <RecentlyViewed/>
+            <IsoCMSTestimonials/>
+        </main>
     )
 }
 
